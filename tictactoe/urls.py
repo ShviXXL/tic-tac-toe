13f2986db0d493cb14910924bbb7653b658cba67
ShviXXL <shvix@pm.me>
2020-12-20 22:29:17 +0300

    Some things

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -1,4 +1,4 @@
-"""tictactoe URL Configuration
+"""tictac URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tictactoe.core.urls')),
-    path('', include('tictactoe.users.urls')),
+    path('', include('tictac.core.urls')),
+    path('', include('tictac.users.urls')),
 ]
